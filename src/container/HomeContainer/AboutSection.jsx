e1337c29ae6dcdd5f334659e0703b597085ea9dc
hr-sobuj <sobujhd@gmail.com>
2023-10-30 01:21:26 +0600

    about us initial view

--- src/container/HomeContainer/AboutSection.jsx
+++ src/container/HomeContainer/AboutSection.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import aboutUsImage from "../../assets/images/about.png";
+import styles from "../../styles/AboutSection.module.css";
 
-function AboutSection() {
-  return <div>AboutSection</div>;
+function AboutUsSection() {
+  return (
+    <section className={styles.aboutUs}>
+      <div className="container">
+        <div className={styles.aboutUsContent}>
+          <div className={styles.aboutUsText}>
+            <h2 className={styles.aboutUsTitle}>About Us</h2>
+            <p className={styles.aboutUsDescription}>
+              We are a passionate team dedicated to delivering top-notch
+              products and services to our customers. With years of experience
+              and a commitment to excellence, we strive to make a difference in
+              every project we undertake.
+            </p>
+          </div>
+          <div className={styles.aboutUsImage}>
+            <img
+              src={aboutUsImage}
+              alt="About Us"
+              className={styles.aboutUsImage}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default AboutSection;
+export default AboutUsSection;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import AboutSection from "../container/HomeContainer/AboutSection";
 import HeroSection from "../container/HomeContainer/HeroSection";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
+      <AboutSection />
     </>
   );
 }
